25a54d0e70ff874369a5cef16473bf4f37b94d00
DESKTOP-O091P98\juliu <julius.talacka@gmail.com>
2022-08-07 14:49:04 +0300

    started project

---
+++ src/main/java/com/uniquex/studentsorting/StudentSortingApplication.java
@@ -0,0 +1,13 @@
+package com.uniquex.studentsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSortingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniquex/studentsorting/StudentSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquex.studentsorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
